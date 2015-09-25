c3b30d7656c3ed3e0503c1d0567d7a1cae168361
Emiliano G. Molina <emiliano.g.molina@gmail.com>
2015-09-25 10:02:15 -0300

    enable header based authorization

--- index.php
+++ index.php
@@ -3,23 +3,21 @@
 namespace MediaInspector;
 
 use MediaInspector\Exception;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once __DIR__.'/vendor/autoload.php'; 
 
 $app = new \Silex\Application(); 
-$app->get('/media/{id}', function($id) use($app) {
+$app->get('/media/{id}', function($id, Request $request) use($app) {
     try {
-        // $authorization_header = $app->request->headers->get('Authorization');
+        $authorization_header = $request->headers->get('Authorization');
 
-        // $app['monolog']->addInfo(sprintf("Authorization: '%s'", $authorization_header));
+        if (strpos($authorization_header, 'Bearer') === false and
+            strpos($authorization_header, 'bearer') === false ) {
+            return $app->json(['error' => 'Invalid Token'], 401);
+        }
 
-        // if (strpos($authorization_header, 'Bearer') === false or
-        //     strpos($authorization_header, 'bearer') === false ) {
-        //     return $app->json('Invalid Token', 401);
-        // }
-
-        // $access_token = substr($authorization_header, 0, strlen('bearer '));
-        $access_token = '37946071.de0b35a.4ad6041a05454637ad4f04f80f530841';
+        $access_token = substr($authorization_header, strlen('bearer '));
 
         $media_inspector = MediaInspectorFactory::create($access_token);
         $media_info = $media_inspector->getMediaInfo($id);
