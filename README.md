32a7a8ba30002a5d36fbea3bd026e13ee2cd66dc
Emiliano G. Molina <emiliano.g.molina@gmail.com>
2015-09-25 10:32:06 -0300

    remove harcoded access token from unit tests

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # media-inspector
 
+Docker Ready !
+
+You need to install Docker in your host
+
+http://docs.docker.com/installation/ubuntulinux/
+
 Download development environment
 
 >> docker pull quay.io/emigm/php-composer
@@ -19,24 +25,32 @@ Update Composer dependencies
 Run UnitTests
 
 >> docker run \
-    -v $(pwd):/var/www quay.io/emigm/php-composer
+    -e TEST_INSTAGRAM_ACCESS_TOKEN=37946071.de0b35a.4ad6041a05454637ad4f04f80f530841 \
+    -v $(pwd):/var/www quay.io/emigm/php-composer \
     "./vendor/bin/phpunit tests"
 
-Run Service
+Run the service from the development environment
 
 >> docker run \
     -e GOOGLE_API_KEY=AIzaSyAJZS9xjpoN_CT7__iaqsxflGleHgL4QhA \
     -e GOOGLE_ENDPOINT=https://maps.googleapis.com \
     -e INSTAGRAM_ENDPOINT=https://api.instagram.com \
+    --name media_inspector_devenv \
     -v $(pwd):/var/www quay.io/emigm/php-composer
     "php -S 0.0.0.0:80"
 
+Run the service from the released version
+
+>> docker run \
+    -e GOOGLE_API_KEY=AIzaSyAJZS9xjpoN_CT7__iaqsxflGleHgL4QhA \
+    -e GOOGLE_ENDPOINT=https://maps.googleapis.com \
+    -e INSTAGRAM_ENDPOINT=https://api.instagram.com \
+    --name media_inspector \
+    quay.io/emigm/media-inspector
+
 Check if service is up and running
 
->> docker ps -q
-${container-id}
-
->> docker exec ${container-id} ip addr show eth0
+>> docker exec media_inspector ip addr show eth0
 ${eth0-ipv4}
 
 >> curl -XGET -H "Authorization: Bearer ${Instagram-Access-Token}" http://${eth0-ipv4}:80/media/{id}
--- tests/unit_tests/RESTClientTest.php
+++ tests/unit_tests/RESTClientTest.php
@@ -6,28 +6,28 @@ use MediaInspector\RESTAdapter;
 
 class RESTClientTest extends \PHPUnit_Framework_TestCase
 {
-    const ACCESS_TOKEN = '37946071.de0b35a.4ad6041a05454637ad4f04f80f530841';
-
     /**
      * @expectedException           Exception 
      * @expectedExceptionMessage    Not implemented
      */
     public function testPost()
     {
+        $access_token = getenv('TEST_INSTAGRAM_ACCESS_TOKEN');
         $client = new RESTAdapter\RESTClient('https://api.instagram.com');
 
         $URI = '/v1/locations/614396723';
-        $QUERY = ['access_token' => self::ACCESS_TOKEN];
+        $QUERY = ['access_token' => $access_token];
 
         $response = $client->post($URI, $QUERY);
     }
 
     public function testGet()
     {
+        $access_token = getenv('TEST_INSTAGRAM_ACCESS_TOKEN');
         $client = new RESTAdapter\RESTClient('https://api.instagram.com');
 
         $URI = '/v1/locations/614396723';
-        $QUERY = ['access_token' => self::ACCESS_TOKEN];
+        $QUERY = ['access_token' => $access_token];
         $HEADERS = ['Accept' => 'application/json'];
         $TIMEOUT = 3.0;
 
@@ -43,10 +43,11 @@ class RESTClientTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetClientError()
     {
+        $access_token = getenv('TEST_INSTAGRAM_ACCESS_TOKEN');
         $client = new RESTAdapter\RESTClient('https://api.instagram.com');
 
         $INVALID_URI = '/v1/invalid/614396723';
-        $QUERY = ['access_token' => self::ACCESS_TOKEN];
+        $QUERY = ['access_token' => $access_token];
         $HEADERS = ['Accept' => 'application/json'];
         $TIMEOUT = 3.0;
 
@@ -59,10 +60,11 @@ class RESTClientTest extends \PHPUnit_Framework_TestCase
      */
     public function testPut()
     {
+        $access_token = getenv('TEST_INSTAGRAM_ACCESS_TOKEN');
         $client = new RESTAdapter\RESTClient('https://api.instagram.com');
 
         $URI = '/v1/locations/614396723';
-        $QUERY = ['access_token' => self::ACCESS_TOKEN];
+        $QUERY = ['access_token' => $access_token];
 
         $response = $client->put($URI, $QUERY);
     }
@@ -73,10 +75,11 @@ class RESTClientTest extends \PHPUnit_Framework_TestCase
      */
     public function testDelete()
     {
+        $access_token = getenv('TEST_INSTAGRAM_ACCESS_TOKEN');
         $client = new RESTAdapter\RESTClient('https://api.instagram.com');
 
         $URI = '/v1/locations/614396723';
-        $QUERY = ['access_token' => self::ACCESS_TOKEN];
+        $QUERY = ['access_token' => $access_token];
 
         $response = $client->delete($URI, $QUERY);
     }
